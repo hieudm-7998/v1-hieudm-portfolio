055cdc347cb740c4d6eb57a36c7ac048c92ce193
Hieu Do <83936407+hieudm-7998@users.noreply.github.com>
2021-10-28 16:22:33 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+  $(".skill-slide").slick({
+    dots: false,
+  });
+  $("#html5").circleProgress({
+    value: 0.8,
+    size: 100,
+    fill: {
+      gradient: ["#cc2027", "#e0811b", "#f6eb0f"],
+    },
+    startAngle: -1.55,
+  });
+  $("#scss").circleProgress({
+    value: 0.8,
+    size: 100,
+    fill: {
+      gradient: ["#cc2027", "#e0811b", "#f6eb0f"],
+    },
+    startAngle: -1.55,
+  });
+  $("#js").circleProgress({
+    value: 0.5,
+    size: 100,
+    fill: {
+      gradient: ["#cc2027", "#e0811b", "#f6eb0f"],
+    },
+    startAngle: -1.55,
+  });
+  $("#vuejs").circleProgress({
+    value: 0.3,
+    size: 100,
+    fill: {
+      gradient: ["#cc2027", "#e0811b", "#f6eb0f"],
+    },
+    startAngle: -1.55,
+  });
+  $("#communication").circleProgress({
+    value: 0.8,
+    size: 100,
+    fill: {
+      gradient: ["#cc2027", "#e0811b", "#f6eb0f"],
+    },
+    startAngle: -1.55,
+  });
+  $("#team-work").circleProgress({
+    value: 0.8,
+    size: 100,
+    fill: {
+      gradient: ["#cc2027", "#e0811b", "#f6eb0f"],
+    },
+    startAngle: -1.55,
+  });
+  $("#self-learning").circleProgress({
+    value: 0.8,
+    size: 100,
+    fill: {
+      gradient: ["#cc2027", "#e0811b", "#f6eb0f"],
+    },
+    startAngle: -1.55,
+  });
+  $("#language").circleProgress({
+    value: 0.8,
+    size: 100,
+    fill: {
+      gradient: ["#cc2027", "#e0811b", "#f6eb0f"],
+    },
+    startAngle: -1.55,
+  });
+  $("#creative").circleProgress({
+    value: 0.8,
+    size: 100,
+    fill: {
+      gradient: ["#cc2027", "#e0811b", "#f6eb0f"],
+    },
+    startAngle: -1.55,
+  });
+});
